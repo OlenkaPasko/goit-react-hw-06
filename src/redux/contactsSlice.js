31c819af816894ab089e80d234c9770463d90751
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-07-24 22:58:31 +0300

    initialState

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,17 +1,25 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = { contacts: [], filter: "" };
+
 const contactsSlice = createSlice({
   name: "contacts",
   // Початковий стан редюсера слайсу
-  initialState: [],
+  initialState,
   reducers: {
-    addContact(state, action) {},
-      deleteContact(state, action) { },
-     filterContact(state, action) {}
+      addContact(state, action) {
+          console.log(action);
+          state.contacts.push(action.payload);
+      },
+      deleteContact(state, action) { 
+          
+      },
+      searchContact(state, action)
+      { state.filter = action.payload; }
   },
 });
 // Генератори екшенів
-export const { addContact, deleteContact, filterContact } = contactsSlice.actions;
+export const { addContact, deleteContact, searchContact } = contactsSlice.actions;
 
 // Редюсер слайсу
 export default contactsSlice.reducer;
