2967a51af5770eea8559c1ceb04adcc4c6db3d92
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-07-25 16:02:06 +0300

    ContactForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,29 +1,33 @@
+import { useDispatch } from "react-redux";
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import { contactsSlice } from "../../redux/contactsSlice";
 import * as Yup from "yup";
-import css from "./ContactForm.module.css"
-  
+
+import css from "./ContactForm.module.css";
 
 const validationSchema = Yup.object({
-    name: Yup.string()
-      .required("Name is required")
-      .min(3, "Name must be at least 3 characters long"),
-    number: Yup.string()
-      .required("Required")
-      .matches(/^[0-49]{50}$/, "Number must be exactly 50 digits"),
+  name: Yup.string()
+    .required("Name is required")
+    .min(3, "Name must be at least 3 characters long"),
+  number: Yup.string()
+    .required("Required")
+    .matches(/^[0-49]{50}$/, "Number must be exactly 50 digits"),
 });
-  
 
 export default function ContactForm({ onSubmitForm }) {
+  const dispatch = useDispatch();
+
   const initialValues = {
     name: "",
     number: "",
   };
 
-  const handleSubmit = (value, { resetForm }) => {
-    //console.log(value);
-    onSubmitForm(value);
-    resetForm();
-  };
+   const handleSubmit = (event) => {
+     event.preventDefault();
+     const form = event.target;
+     dispatch(contactsSlice(form.elements.text.value));
+     form.reset();
+   };
 
   return (
     <Formik
@@ -44,14 +48,17 @@ export default function ContactForm({ onSubmitForm }) {
         </label>
         <label className={css.label}>
           Number:
-          <Field className ={css.field}
+          <Field
+            className={css.field}
             type="tel"
             name="number"
             title="Please enter your phone number"
             required
           />
         </label>
-        <button type="submit" className={css.button}>Add contact</button>
+        <button type="submit" className={css.button}>
+          Add contact
+        </button>
       </Form>
     </Formik>
   );
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -4,22 +4,31 @@ const initialState = { contacts: [], filter: "" };
 
 const contactsSlice = createSlice({
   name: "contacts",
+  
   // Початковий стан редюсера слайсу
   initialState,
+
   reducers: {
-      addContact(state, action) {
-          console.log(action);
-          state.contacts.push(action.payload);
-      },
-      deleteContact(state, action) { 
-          
-      },
-      searchContact(state, action)
-      { state.filter = action.payload; }
+    addContact(state, action) {
+      console.log(action);
+      state.contacts.push(action.payload);
+    },
+    deleteContact(state, action) {
+      state.contacts = state.contacts.filter(
+        (contact) => contact.id !== action.payload
+      );
+    },
+    searchContact(state, action) {
+      state.filter = action.payload;
+    },
   },
 });
-// Генератори екшенів
-export const { addContact, deleteContact, searchContact } = contactsSlice.actions;
+// Генератори екшенів для використання в dispatch
+export const { addContact, deleteContact, searchContact } =
+  contactsSlice.actions;
+
+// Функція-селектор для використання в useSelector
+export const selectContacts = (state) => state.contacts;
 
 // Редюсер слайсу
 export default contactsSlice.reducer;
