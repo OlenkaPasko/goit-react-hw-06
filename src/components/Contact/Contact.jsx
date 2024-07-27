74ef7eb004980e19013245d763194697991fa8eb
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-07-27 08:05:58 +0300

    filter

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,14 +1,19 @@
 import { useDispatch, useSelector } from "react-redux";
 import { deleteContact } from "../../redux/contactsSlice";
-import  ContactList   from "../ContactList/ContactList";
+import ContactList from "../ContactList/ContactList";
 
 export default function ContactsContainer() {
   const dispatch = useDispatch();
   const contacts = useSelector((state) => state.contacts.items);
+  const filter = useSelector((state) => state.filters.name);
 
   const handleDeleteContact = (id) => {
     dispatch(deleteContact(id));
   };
-
-  return <ContactList contacts={contacts} onDelete={handleDeleteContact} />;
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+  return (
+    <ContactList contacts={filteredContacts} onDelete={handleDeleteContact} />
+  );
 }
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,24 @@
 import { useDispatch, useSelector } from "react-redux";
 import { deleteContact, selectContacts } from "../../redux/contactsSlice";
 
+import { selectNameFilter } from "../../redux/filtersSlice";
+
 const ContactList = () => {
   const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
   const dispatch = useDispatch();
-
+ 
   const handleDelete = (id) => {
-    dispatch(deleteContact(id));
-  };
+   dispatch(deleteContact(id));
+ };
+
+ const filteredContacts = contacts.filter((contact) =>
+   contact.name.toLowerCase().includes(filter.toLowerCase())
+ );
 
   return (
     <ul>
-      {contacts.map((item) => (
+      {filteredContacts.map((item) => (
         <li key={item.id}>
           <p>
             {item.name}: {item.number}
