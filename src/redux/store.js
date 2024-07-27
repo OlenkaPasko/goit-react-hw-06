e45cd45c8d4f0e940039ee1710e4ec5ec9c2cea3
OlenkaPasko <113981072+OlenkaPasko@users.noreply.github.com>
2024-07-27 08:32:22 +0300

    persisted

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit";
-//import { persistStore, persistReducer } from 'redux-persist'
-//import storage from 'redux-persist/lib/storage' 
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' 
 import contactsReducer from "./contactsSlice";
 import filtersReducer from "./filtersSlice";
 
+// Збереження поля items зі слайса контактів
+const persistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ['items'],
+};
+
+const persistedContactsReducer = persistReducer(persistConfig, contactsReducer);
+
 const store = configureStore({
   reducer: {
-    contacts: contactsReducer,
+    contacts: persistedContactsReducer,
     filters: filtersReducer,
   },
 });
+export const persistor = persistStore(store);
 
 export default store;
